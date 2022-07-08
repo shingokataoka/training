75272bec6d5b4f36811dd5f6790123b0fd1eb0d5
shingokataoka <shingo.kataoka777@gmail.com>
2022-07-08 21:21:31 +0900

    ClassBase

--- src/resources/views/tests/component-test2.blade.php
+++ src/resources/views/tests/component-test2.blade.php
@@ -1,5 +1,8 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー２</x-slot>
-    
+
     コンポーネントテスト２
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージ２" defaultMessage="初期値から変更しています。" />
 </x-tests.app>
