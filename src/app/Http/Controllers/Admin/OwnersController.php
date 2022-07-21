c2a6104371529df87e855c7c251288a848a9c1d8
shingokataoka <shingo.kataoka777@gmail.com>
2022-07-21 12:25:12 +0900

    Store

--- src/app/Http/Controllers/Admin/OwnersController.php
+++ src/app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Owner;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class OwnersController extends Controller
 {
@@ -46,7 +47,21 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+            ->route('admin.owners.index')
+            ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
