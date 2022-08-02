2a674fee86ca47d25d05f47a112a78cd359192d1
shingokataoka <shingo.kataoka777@gmail.com>
2022-08-02 18:31:27 +0900

    category

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             ShopSeeder::class,
             UserSeeder::class,
+            ImageSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
