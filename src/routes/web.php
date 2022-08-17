916f3118f4cc19863271ddc396bf2895ba585df0
shingokataoka <shingo.kataoka777@gmail.com>
2022-08-17 23:27:04 +0900

    cart delete

--- src/routes/web.php
+++ src/routes/web.php
@@ -36,9 +36,12 @@ Route::middleware('auth:users')->group(function() {
 Route::prefix('cart')->middleware('auth:users')->group(function() {
     Route::get('/', [CartController::class, 'index'])
         ->name('cart.index');
-        
+
         Route::post('/add', [CartController::class, 'add'])
         ->name('cart.add');
+
+        Route::delete('/{item}/delete', [CartController::class, 'delete'])
+            ->name('cart.delete');
 });
 
 
